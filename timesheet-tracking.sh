c523b088819588d5626269ef6d6c7d0cb8d5a21f
Sean Wisely <s.wisely@nib.com.au>
2024-07-29 21:16:14 +1000

    Improve script to write start/end
    Initial script was just a log of Lock and Unlock events.
    This script now writes a csv with session start and end times which can
    then be parsed to processed more later to get length etc.

--- timesheet-tracking.sh
+++ timesheet-tracking.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 # This is based on https://stackoverflow.com/a/66723000
@@ -8,10 +8,13 @@ function screenIsUnlocked { [ "$(/usr/libexec/PlistBuddy -c "print :IOConsoleUse
 # This comes from https://community.jamf.com/t5/jamf-pro/check-if-screen-is-locked-in-a-script/m-p/116688
 IDLE_TIME=$((`ioreg -c IOHIDSystem | sed -e '/HIDIdleTime/ !{ d' -e 't' -e '}' -e 's/.* = //g' -e 'q'` / 1000000000))
 
-TIME_LOG_FILE=~/timesheet.csv
+TIME_LOG_FILE="${1:-$PWD/timesheet.csv}"
+
+echo ""
+echo "Writing timesheet file to $TIME_LOG_FILE"
 
 if [ ! -f "$TIME_LOG_FILE" ]; then
-    echo "Date,Time,Status" > "$TIME_LOG_FILE"
+    echo "Date,Session Start,Session End" > "$TIME_LOG_FILE"
 fi
 
 DATE=$(date -I)
@@ -19,44 +22,44 @@ TIME=$(date +%H:%M)
 
 LAST_LINE=$(tail -n 1 "$TIME_LOG_FILE")
 
-LAST_DATE=$(cut -d',' -f1 <<< $LAST_LINE)
-LAST_STATUS=$(cut -d',' -f3 <<< $LAST_LINE)
+IFS=',' read -r -a LINE_PARTS <<< "$LAST_LINE"
+
+LAST_DATE="${LINE_PARTS[0]}"
 
 USER_IS_IDLE=$([ $IDLE_TIME -gt 300 ] && echo true || echo false)
 
-echo ""
+LAST_SESSION_STATUS="Unknown"
+if [ ${#LINE_PARTS[@]} == 2 ]; then
+    LAST_SESSION_STATUS="Open"
+elif [ ${#LINE_PARTS[@]} == 3 ]; then
+    LAST_SESSION_STATUS="Ended"
+fi
+
 echo "Current date: $DATE"
 echo "Current time: $TIME"
 echo "Last date: $LAST_DATE"
-echo "Last status: $LAST_STATUS"
+echo "Last session status: $LAST_SESSION_STATUS"
 echo "Current idle time: $IDLE_TIME"
 echo "User is idle: $USER_IS_IDLE"
 
-if [ "$DATE" != "$LAST_DATE" ]; then
-    if screenIsLocked || [ $USER_IS_IDLE = true ] ; then
-        echo "New day but user inactive, aborting..."
-        exit 0
-    else
-        echo "New day and user active, writing..."
-        echo "$DATE,$TIME,Unlocked" >> "$TIME_LOG_FILE"
-        exit 0
-    fi
-fi
-
 if screenIsLocked; then
     echo "Screen is locked..."
-    if [ "$LAST_STATUS" != "Locked" ]; then
-        echo "Last entry was unlocked, writing..."
-        echo "$DATE,$TIME,Locked" >> "$TIME_LOG_FILE"
+    if [ "$LAST_SESSION_STATUS" != "Ended" ] && [ "$DATE" == "$LAST_DATE" ]; then
+        echo "Last session still open, writing..."
+        echo ",$TIME" >> "$TIME_LOG_FILE"
     else
-        echo "Last entry was locked, skipping..."
+        echo "Last session already marked as ended, skipping..."
     fi
 elif screenIsUnlocked; then
     echo "Screen is unlocked..."
-    if [ "$LAST_STATUS" != "Unlocked" ]; then
-        echo "Last entry was locked, writing..."
-        echo "$DATE,$TIME,Unlocked" >> "$TIME_LOG_FILE"
+    if [ "$LAST_SESSION_STATUS" != "Open" ]; then
+        echo "Last session ended, writing..."
+        echo -n "$DATE,$TIME" >> "$TIME_LOG_FILE"
+    elif [ "$LAST_DATE" != "$DATE" ]; then
+        echo "Last session was on different date, writing..."
+        echo "" >> "$TIME_LOG_FILE"
+        echo -n "$DATE,$TIME" >> "$TIME_LOG_FILE"
     else
-        echo "Last entry was unlocked, skipping..."
+        echo "Last session still open, skipping..."
     fi
 fi
